abcd3025d15a5778b5f2de97d3470637a52c3d6e
Gabi8086 <112909125+Gabi8086@users.noreply.github.com>
2022-11-04 19:41:03 -0300

    Add files via upload

---
+++ sla/sla/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            String endereço;
+            String telefone;
+
+            Console.WriteLine("colocar nome:");
+            nome = Console.ReadLine();
+            Console.WriteLine("colocar endereço: ");
+            endereço = Console.ReadLine();
+            Console.WriteLine("telefone: ");
+            telefone = Console.ReadLine();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("nome:");
+            Console.WriteLine(nome);
+            Console.WriteLine("endereço: ");
+            Console.WriteLine(endereço);
+            Console.WriteLine("telefone: ");
+            Console.WriteLine(telefone);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
